76135f4e92d6405abcdb74b93b4047f22fe78510
lbxz <957062024@qq.com>
2019-01-03 17:26:47 +0800

    build commit

---
+++ build/precache-manifest.07dd86761d28e624c00b101dc716a446.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "df609d3b31c51a1e3b85",
+    "url": "/static/js/main.df609d3b.chunk.js"
+  },
+  {
+    "revision": "fa92c112869d3657d47b",
+    "url": "/static/js/1.fa92c112.chunk.js"
+  },
+  {
+    "revision": "df609d3b31c51a1e3b85",
+    "url": "/static/css/main.43a25593.chunk.css"
+  },
+  {
+    "revision": "19675d0d7338fd94b03a7a08865882da",
+    "url": "/index.html"
+  }
+];
