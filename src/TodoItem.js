8af09d92707e3104293d771fd5ae972468a8ae9f
lbxz <957062024@qq.com>
2019-01-05 13:18:31 +0800

    代码优化

--- src/TodoItem.js
+++ src/TodoItem.js
@@ -3,12 +3,21 @@ import React from 'react';
 class TodoItem extends React.Component{
     //子组件如果想和父组件通信，子组件要调用父组件传递过来的方法
 
+    constructor(props) {
+        super(props);
+
+        this.handleDelete = this.handleDelete.bind(this);
+    }
+
     handleDelete() {
+        // const { delete, index} = this.props;
+        // delete(index);
         this.props.delete(this.props.index);
     }
     render(){
+        const { content } = this.props;
         return (
-            <div onClick={this.handleDelete.bind(this)}>{this.props.content}</div>
+            <div onClick={this.handleDelete}>{content}</div>
         )
     }
 }
--- src/TodoList.js
+++ src/TodoList.js
@@ -8,6 +8,10 @@ class TodoList extends Component {
           list: [],
           inputvalue: ''
       }
+
+      this.handleInputChange = this.handleInputChange.bind(this);
+      this.handleBtnClick = this.handleBtnClick.bind(this);
+      this.handleDelete = this.handleDelete.bind(this);
   }
 
   handleBtnClick(){
@@ -23,13 +27,13 @@ class TodoList extends Component {
       })
   }
 
-  handleItemClick(index){
-      const list = [...this.state.list];
-      list.splice(index, 1);
-      this.setState({
-          list: list
-      })
-  }
+  // handleItemClick(index){
+  //     const list = [...this.state.list];
+  //     list.splice(index, 1);
+  //     this.setState({
+  //         list: list
+  //     })
+  // }
 
   handleDelete(index) {
       const list = [...this.state.list];
@@ -39,23 +43,29 @@ class TodoList extends Component {
       })
   }
 
+  getTdoItems() {
+      return (
+          this.state.list.map((item, index) => {
+              return (<TodoItem
+                  delete={this.handleDelete}
+                  key={index}
+                  content={item}
+                  index={index}/>)
+              // return <li key={index} onClick={this.handleItemClick.bind(this, index)}>{item}</li>
+          })
+      )
+  }
+
   //父组件通过属性的形式向子组件传递参数
   //子组件通过props接受父组件传递过来的参数
   render() {
     return (
       <div>
         <div>
-          <input value={this.state.inputvalue} onChange={this.handleInputChange.bind(this)}/>
-          <button onClick={this.handleBtnClick.bind(this)}>add</button>
+          <input value={this.state.inputvalue} onChange={this.handleInputChange}/>
+          <button onClick={this.handleBtnClick}>add</button>
         </div>
-        <ul>
-          {
-              this.state.list.map((item, index) => {
-                  return <TodoItem delete={this.handleDelete.bind(this)} key={index} content={item} index={index}/>
-                  // return <li key={index} onClick={this.handleItemClick.bind(this, index)}>{item}</li>
-              })
-          }
-        </ul>
+        <ul>{this.getTdoItems()}</ul>
       </div>
     );
   }
