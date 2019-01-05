2b48c07dd90b08bb7a96ffd48072d40230aa18b3
lbxz <957062024@qq.com>
2019-01-05 12:33:17 +0800

    todolist删除功能

--- src/TodoList.js
+++ src/TodoList.js
@@ -4,30 +4,43 @@ class TodoList extends Component {
   constructor(props) {
       super(props);
       this.state = {
-          list: [
-              'learn react',
-              'learn vue',
-              'learn python'
-          ]
+          list: [],
+          inputvalue: ''
       }
   }
 
   handleBtnClick(){
     this.setState({
-        list: [...this.state.list, 'learn spring boot']
+        list: [...this.state.list, this.state.inputvalue],
+        inputvalue: ''
     })
   }
+
+  handleInputChange(e) {
+      this.setState({
+          inputvalue: e.target.value
+      })
+  }
+
+  handleItemClick(index){
+      const list = [...this.state.list];
+      list.splice(index, 1);
+      this.setState({
+          list: list
+      })
+  }
+
   render() {
     return (
       <div>
         <div>
-          <input/>
+          <input value={this.state.inputvalue} onChange={this.handleInputChange.bind(this)}/>
           <button onClick={this.handleBtnClick.bind(this)}>add</button>
         </div>
         <ul>
           {
               this.state.list.map((item, index) => {
-                  return <li key={index}>{item}</li>
+                  return <li key={index} onClick={this.handleItemClick.bind(this, index)}>{item}</li>
               })
           }
         </ul>
